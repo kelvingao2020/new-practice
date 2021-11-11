6bfb1c7fa0f440908e634692692656cbcc63930c
kelvingao2015 <kelvingao2015@gmail.com>
2021-11-10 18:57:49 -0800

    update

---
+++ src/main/java/com/kelvingao/gitandgithub/practicegitgithubproject/PracticeGitGithubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kelvingao.gitandgithub.practicegitgithubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGitGithubProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeGitGithubProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kelvingao/gitandgithub/practicegitgithubproject/PracticeGitGithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvingao.gitandgithub.practicegitgithubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGitGithubProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
